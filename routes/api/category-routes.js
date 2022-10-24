34796d778f0a90c70045b6f1dcf71297c6f29def
kabradshaw1 <itllbok82@gmail.com>
2022-10-24 11:45:39 -0400

    fixed get category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'catagory_id'],
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
       }
     ]
   })
@@ -36,7 +36,7 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'catagory_id'],
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
       }
     ]
   })
